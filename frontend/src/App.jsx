d5457229d9318987bab4dda9684717d43124b1fe
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-03-25 23:11:27 +0545

    Added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import React from 'react'
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Login from './pages/Authentication/Login'
+import Signup from './pages/Authentication/Signup'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-     <div>App</div>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/signup" exact element={<Signup/>} />
+          <Route path="/dashboard" element={<Home/>} />
+          <Route path="/income" element={<Income/>} />
+          <Route path="/expense" element={<Expense/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+const Root=()=>{
+  //check to see if token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  //redirect to dahsboard if authenticated
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+}
---
+++ frontend/src/pages/authentication/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import AuthLayout from '../../components/Layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+
+  const navigate=useNavigate()
+
+  const handleLogin = async (e) => {
+
+  }
+  return (
+    <AuthLayout>
+        <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+            <h3 className='text-xl font-semibold text-black'>Welcome back</h3>
+            <p className='text-xs text-slate-700 mt-[5px] mb-6 '>
+                Please enter your details to log in
+            </p>
+            <form onSubmit={handleLogin}>
+              <input
+                type="text"
+                placeholder="john@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className=''/>
+            </form>
+        </div>
+    </AuthLayout>
+  )
+}
+
+export default Login
