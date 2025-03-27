52b3bbf5b80fb9f020f987fdf01f24226626096a
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-03-27 22:18:37 +0545

    Added Signup

--- frontend/src/components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { LuUser,LuUpload,LuTrash } from 'react-icons/lu'
+
 
 const ProfilePhotoSelector = ({image,setImage}) => {
+  const inputRef=useRef(null)
+  const [previewUrl,setPreviewUrl]=useState(null)
+  
+  const handleImageChange=(event)=>{
+    const file=event.target.files[0]
+    if(file){
+      setImage(file)
+      const preview = URL.createObjectURL(file)
+      setPreviewUrl(preview)
+    }
+  }
+  const handleRemoveImage=()=>{
+    setImage(null)
+    setPreviewUrl(null)
+  }
+  const onChooseFile=()=>{
+    inputRef.current.click()
+  }
   return (
-    <div>ProfilePhotoSelector</div>
+    <div className='flex justify-center mb-6'>
+      <input
+        ref={inputRef}
+        type='file'
+        accept='image/*'
+        className='hidden'
+        onChange={handleImageChange}
+        />
+        {!image?(
+          <div className='w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center relative'>
+            <LuUser className='text-4xl text-primary'/>
+
+            <button
+              type='button'
+              onClick={onChooseFile}
+              className='w-8 h-8 bg-primary text-white flex items-center justify-center rounded-full absolute -bottom-1 -right-1 cursor-pointer'>
+                <LuUpload/>
+              </button>
+          </div>
+        
+          ):(<div className='relative'>
+            <img
+            src={previewUrl}
+            alt='profile picture'
+            className='w-20 h-20 rounded-full object-cover'
+            />
+            <button
+            type='button'
+            onClick={handleRemoveImage}
+            className='w-8 h-8 bg-red-600 text-white flex items-center justify-center rounded-full absolute -bottom-1 -right-1 cursor-pointer'
+            >
+              <LuTrash/>
+            </button>
+
+          </div>
+        )}
+
+    </div>
   )
 }
 
--- frontend/src/pages/authentication/Signup.jsx
+++ frontend/src/pages/authentication/Signup.jsx
@@ -3,6 +3,7 @@ import AuthLayout from '../../components/Layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom'
 import Input from '../../components/Inputs/Input'
 import { validateEmail } from '../../utils/helper'
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector'
 
 
 const Signup = () => {
@@ -15,7 +16,25 @@ const Signup = () => {
 
   const navigate = useNavigate()
   const handleSignup = async (e) => {
-
+    e.preventDefault()
+    if (!fullName) {
+      setError('Your full name is required')
+      return
+    }
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address')
+      return
+    }
+    if (password.length < 8) {
+      setError('Password must be at least 8 characters')
+      return
+    }
+    if (!password) {
+      setError('Password is required')
+      return
+    }
+    setError("")
+    //Signup API call
   }
   return (
     <AuthLayout>
@@ -23,10 +42,10 @@ const Signup = () => {
         <h3 className='text-xl font-semibold text-black'>Create an account</h3>
         <p className='text-xs text-slate-700 mt-[5px] mb-6'>Join us today by entering your details below.</p>
         <form onSubmit={handleSignup}>
-          
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
           <div className='grid grid-cols-2 md:grid-cols-2 gap-4'>
-          <Input
+            <Input
               type="text"
               placeholder="John Doe"
               label="Full Name"
@@ -34,23 +53,32 @@ const Signup = () => {
               onChange={(e) => setFullName(e.target.value)}
             />
             <Input
-                type="text"
-                placeholder="john@example.com"
-                label="Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className=''/>
+              type="text"
+              placeholder="john@example.com"
+              label="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='' />
             <div className='col-span-2'>
               <Input
-                  type="password"
-                  placeholder=""
-                  label="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className=''/>
+                type="password"
+                placeholder=""
+                label="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className='' />
             </div>
-            
+
           </div>
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          <button type='submit' className='btn-primary '>
+            SIGN UP
+          </button>
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Already have an account?
+            <Link className='text-purple-500 cursor-pointer font-medium underline'
+              to='/login'>Login</Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
